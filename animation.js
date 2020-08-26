d82e80ccea4ef5a3c0fca991662dd1a6e5e71bb9
evanmoring <epmoring@gmail.com>
2020-08-26 12:23:46 -0700

    Add files via upload
    Cleaned up parameter names

--- animation.js
+++ animation.js
@@ -1,12 +1,12 @@
-var counter=53
-var plusMinus = 3
+
 var textList =[]
 const start = 36;
 const end = 93;
 const step = 4;
 var animationCheck = true;
-var newNum
-const animationFolder = 'demo/'
+var newNum;
+const animationFolder = 'demo/';
+const msBetweenFrames = 200;
 
 loadCounter = start
 function load(i){
@@ -24,7 +24,7 @@ function load(i){
 function addTxt(){
     textList.push(this.response);
     if(textList.length>=((end-start)/4)){
-        next(0)
+        playFrame(0)
         
         }
     else{
@@ -33,7 +33,7 @@ function addTxt(){
     }
     }
 
-function next(i){    
+function playFrame(i){    
     
     if(animationCheck){
        var pre=document.getElementById('animation');
@@ -43,7 +43,7 @@ function next(i){
     if (newNum>=textList.length){
         newNum = 0;
     } 
-    setTimeout(() => {  next(newNum); }, 200);
+    setTimeout(() => {  playFrame(newNum); }, msBetweenFrames);
         }
     }
 function animationPause(){
@@ -53,7 +53,7 @@ function animationPause(){
     }
     else{
         animationCheck=true;
-        next(newNum)
+        playFrame(newNum)
         document.getElementById('pauseButton').value = 'Pause'
     }
 }
